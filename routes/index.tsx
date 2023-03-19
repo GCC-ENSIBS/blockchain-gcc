b28e80fc3f91d9c53da4da6768b4d7b45f25d96c
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-03-19 17:11:44 +0100

    Trkl

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,6 +18,8 @@ export default function Home() {
         </div>
         <h2 class={"text-center text-2xl font-bold mt-32"}>RPC Endpoint</h2>
         <h2 class={"text-center text-2xl font-italic"}>http://blockchain.gcc-ensibs.fr:8545</h2>
+        <h2 className={"text-center text-2xl font-bold mt-2"}>Chain ID</h2>
+        <h2 className={"text-center text-2xl font-italic"}>12345</h2>
     </>
   );
 }
