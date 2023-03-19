f5b64e5ae990fada45ed19f9f1605ae10d9be450
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-03-19 16:45:08 +0100

    Trkl

--- routes/api/faucet.tsx
+++ routes/api/faucet.tsx
@@ -6,22 +6,24 @@ import { WithSession } from "https://deno.land/x/fresh_session@0.2.0/mod.ts";
 export type Data = { session: Record<string, string> };
 
 export const handler: Handlers<Data, WithSession> = {
-    async POST(req, ctx) {
+    async POST(req, _ctx) {
 
         const body = await req.json();
         const address = body.address;
 
-        const response = await giveETH();
-        return new Response(JSON.stringify({address: address}), {status:200})
+        const response = await giveETH(address);
+        return new Response(JSON.stringify({response: response}), {status:200})
     },
 };
 
-const giveETH = async (address) => {
+const giveETH = async (address: string) => {
+    const gasPrice = await web3.eth.getGasPrice();
     const accounts = await web3.eth.getAccounts();
     await web3.eth.sendTransaction({
         from: accounts[0],
         to: address,
-        value: web3.utils.toWei('1', 'ether')
+        value: web3.utils.toWei('1', 'ether'),
+        gas: gasPrice
     });
-    return address;
+    return "ETH sent to " + address;
 };
