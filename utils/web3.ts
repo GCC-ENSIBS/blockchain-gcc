8250a3844f538b4a316d0da99001dd7db27e786d
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-03-20 11:56:11 +0100

    Blockhain hardhat Config

--- utils/web3.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from "npm:web3";
-import {config} from "./config.ts";
-
-const web3 = new Web3(new Web3.providers.HttpProvider("http://blockchain.gcc-ensibs.fr:8545"))
-const account = web3.eth.accounts.privateKeyToAccount(config.privateKey);
-web3.eth.accounts.wallet.add(account);
-
-export default web3;
